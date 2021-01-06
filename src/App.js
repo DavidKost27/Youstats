67f78cfacbafac74217eb5ad40d09730f9b79112
David Kostuchenko <davidkost1999@gmail.com>
2021-01-06 22:44:41 +0200

    First introductuin to axios and postman usage
    - Used axios
    - Figured out how to use the YouTube API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.scss";
+import "normalize.css";
+import axios from "axios";
 
 function App() {
-  return <div className="App">YouTube Stats</div>;
+  const apiRequestHandler = () => {
+    axios
+      .get(
+        "https://youtube.googleapis.com/youtube/v3/channels?part=statistics&forUsername=Wycc220&key=AIzaSyCCEVLyUMtok_H-b3-Z4hWRIFSsHfmCzTg"
+      )
+      .then((response) => {
+        console.log(response);
+      });
+  };
+  return (
+    <div className="App">
+      YouTube Stats
+      <button onClick={apiRequestHandler}>Get Info</button>
+    </div>
+  );
 }
 
 export default App;
